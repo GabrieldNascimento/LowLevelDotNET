8c527ae25617809bb36d490d172566b071f2032c
Gabriel Nascimento <jpnr09@gmail.com>
2026-04-02 18:18:56 -0300

    First commit - initial console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LowLevelDotNET
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
